90bef2f4d48a199028864ba9ad7b85db8aea95da
AbelRomann <abel.rafael.roman@gmail.com>
2026-02-20 20:34:30 -0400

    Fix compose imports and player progress bindings

--- app/src/main/java/com/example/reproductor/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/reproductor/presentation/screens/home/HomeScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.RowScope.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -44,7 +44,6 @@ import coil.compose.AsyncImage
 import com.example.reproductor.domain.model.Song
 import com.example.reproductor.presentation.library.LibraryViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     onNavigateToPlayer: () -> Unit,
